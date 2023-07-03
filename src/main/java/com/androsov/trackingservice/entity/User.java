14e13b06d8eb0bdeecff1dc7a782e8ab05fa8e7e
dictator_zx <ivanandrosovv@gmail.com>
2023-07-03 19:40:03 +0300

    Setted properties and created user entity

---
+++ src/main/java/com/androsov/trackingservice/entity/User.java
@@ -0,0 +1,22 @@
+package com.androsov.trackingservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+@Table(name = "User_details")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String authorities; // authorities stored like "ROLE_USER,ROLE_ADMIN"
+}
