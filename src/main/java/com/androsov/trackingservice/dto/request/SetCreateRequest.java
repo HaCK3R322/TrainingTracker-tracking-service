0785c60b161c262dadb52845eb83c168cdb2795c
dictator_zx <ivanandrosovv@gmail.com>
2023-07-07 18:45:50 +0300

    Fixed SetCreateRequest

--- src/main/java/com/androsov/trackingservice/dto/request/SetCreateRequest.java
+++ src/main/java/com/androsov/trackingservice/dto/request/SetCreateRequest.java
@@ -3,10 +3,13 @@ package com.androsov.trackingservice.dto.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Data
 @NoArgsConstructor
 public class SetCreateRequest {
     private Long exerciseId;
     private Integer amount;
     private Integer reps;
+    private Timestamp timestamp;
 }
