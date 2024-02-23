c9a4569aa21bdaf951e839ba28441bdf22b9e74a
dictator_zx <ivanandrosovv@gmail.com>
2024-02-24 00:17:15 +0300

    Added getAllByTrainingIdWithSets mapping

--- src/main/java/com/androsov/trackingservice/controller/ExerciseController.java
+++ src/main/java/com/androsov/trackingservice/controller/ExerciseController.java
@@ -1,17 +1,24 @@
 package com.androsov.trackingservice.controller;
 
 import com.androsov.trackingservice.dto.converter.ExerciseToDtoConverter;
+import com.androsov.trackingservice.dto.converter.ExerciseToExerciseWithSetsDtoConverter;
 import com.androsov.trackingservice.dto.request.ExerciseCreateRequest;
 import com.androsov.trackingservice.dto.response.exercise.ExerciseDtoResponse;
+import com.androsov.trackingservice.dto.response.exercise.ExerciseWithSetsDtoResponse;
 import com.androsov.trackingservice.entity.Exercise;
+import com.androsov.trackingservice.entity.Set;
 import com.androsov.trackingservice.service.ExerciseService;
+import com.androsov.trackingservice.service.SetService;
+import com.google.common.collect.Sets;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/tracking/exercises")
@@ -21,6 +28,8 @@ public class ExerciseController {
     private final ExerciseService exerciseService;
 
     private final ExerciseToDtoConverter exerciseToDtoConverter;
+    private final ExerciseToExerciseWithSetsDtoConverter exerciseToExerciseWithSetsDtoConverter;
+    private final SetService setService;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ExerciseDtoResponse> createExercise(@RequestBody ExerciseCreateRequest request) {
@@ -34,6 +43,22 @@ public class ExerciseController {
         List<Exercise> exercises = exerciseService.findAllByTrainingId(trainingId);
         List<ExerciseDtoResponse> exerciseDtoResponses = exerciseToDtoConverter.convert(exercises);
         return ResponseEntity.status(HttpStatus.OK).body(exerciseDtoResponses);
+    }
+
+    @GetMapping(params = {"trainingId", "withSets"})
+    public ResponseEntity<List<ExerciseWithSetsDtoResponse>> getAllByTrainingIdWithSets(@RequestParam Long trainingId, @RequestParam Boolean withSets) {
+        List<Exercise> exercises = exerciseService.findAllByTrainingId(trainingId);
+
+        Map<Exercise, List<Set>> map = new HashMap<>();
+
+        for (Exercise exercise: exercises) {
+            List<Set> sets = setService.findAllByExerciseId(exercise.getId());
+            map.put(exercise, sets);
+        }
+
+        List<ExerciseWithSetsDtoResponse> response = exerciseToExerciseWithSetsDtoConverter.convert(map);
+
+        return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping(params = "exerciseId")
